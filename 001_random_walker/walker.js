082df14a224d7a51ff8444d1f45a1517cd0bb25a
Millan Castro <m.castrovilarino@gmail.com>
2023-06-07 07:28:02 +0200

    Random Walker: tendency to go right and bottom

--- 001_random_walker/walker.js
+++ 001_random_walker/walker.js
@@ -8,6 +8,7 @@ class Walker {
     constructor() {
         this.x = width / 2;
         this.y = height / 2;
+        this.prob = [-1, 1, 1]
     }
   
     display() {
@@ -17,10 +18,10 @@ class Walker {
   
     step() {
         /* allows the walker to step randomly to any of the 8 pixels surrounding it */
-        let step_x = (random(-1,1));
-        let step_y= (random(-1,1));
+        let index_x = floor(random(this.prob.length));
+        let index_y= floor(random(this.prob.length));
         
-        this.x += step_x
-        this.y += step_y
+        this.x += this.prob[index_x]
+        this.y += this.prob[index_y]
     }
   }
